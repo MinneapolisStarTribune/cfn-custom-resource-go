a11a07d9183c48d133575af1fda29e66781bff4c
Chris Harrington <chris.harrington@startribune.com>
2022-10-12 00:20:26 -0500

    Introduce YAMLBool type

---
+++ yamlbool.go
@@ -0,0 +1,22 @@
+package cfncustomresource
+
+import "fmt"
+
+// YAMLBool represents a boolean value from a YAML template. This type
+// is important because CloudFormation will take a YAML value of true
+// and pass it to your resource as a string, which the json unmarshaler
+// refuses to parse into a bool. In your attribute types, use YAMLBool.
+type YAMLBool bool
+
+// TextUnmarshal implements TextUnmarshaler to satisfy json.Unmarshal.
+func (yb *YAMLBool) TextUnmarshal(b []byte) error {
+	switch string(b) {
+	case "y", "Y", "yes", "Yes", "YES", "true", "True", "TRUE", "on", "On", "ON":
+		*yb = true
+	case "n", "N", "no", "No", "NO", "false", "False", "FALSE", "off", "Off", "OFF":
+		*yb = false
+	default:
+		return fmt.Errorf("cannot parse %q as YAML Bool", string(b))
+	}
+	return nil
+}
